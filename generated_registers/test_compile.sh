ad73db19bf2eb7bf74a47c4717e14505c7c6b268
Sayantan Paul <sayantan.rkmveri@gmail.com>
2025-12-04 13:54:49 +0530

    Delete generated_registers directory

--- generated_registers/test_compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-pdflatex -interaction=nonstopmode "CS101_register.tex"
-if [ -f "CS101_register.pdf" ]; then
-  echo "Success! PDF created: CS101_register.pdf"
-else
-  echo "Compilation failed. Check CS101_register.log for errors."
-fi
